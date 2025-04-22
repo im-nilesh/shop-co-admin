43007addd20328701dd9fd29f939ebcae61271f9
Nilesh <nileshdadab@gmail.com>
2025-04-22 12:18:54 +0530

    Add Firebase SDK and initialize Firebase app
    - Updated package.json to include Firebase SDK version 11.6.0.
    - Created firebase.js service file to initialize Firebase with the provided configuration.

---
+++ src/services/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBO5tJjOunLhHEMlTl7NTaNuHi1r5cDIRM",
+  authDomain: "shop-co-78d16.firebaseapp.com",
+  projectId: "shop-co-78d16",
+  storageBucket: "shop-co-78d16.firebasestorage.app",
+  messagingSenderId: "1030482165306",
+  appId: "1:1030482165306:web:9e00d4975a7e11bef4ff03",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
