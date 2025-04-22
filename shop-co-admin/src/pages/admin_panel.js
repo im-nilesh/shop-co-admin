6c13cf01a4e9f969133ed97104133ea7947fbf31
Nilesh <nileshdadab@gmail.com>
2025-04-22 13:44:31 +0530

    Admin Panel Right Design

--- shop-co-admin/src/pages/admin_panel.js
+++ shop-co-admin/src/pages/admin_panel.js
@@ -1,139 +1,105 @@
 import React from "react";
 import {
-  Box,
-  Typography,
-  Avatar,
+  Drawer,
   List,
   ListItem,
-  ListItemAvatar,
+  ListItemIcon,
   ListItemText,
   Divider,
+  Box,
+  Typography,
 } from "@mui/material";
+import DashboardIcon from "@mui/icons-material/Dashboard";
+import AccountCircleIcon from "@mui/icons-material/AccountCircle";
+import FolderIcon from "@mui/icons-material/Folder";
+import BusinessIcon from "@mui/icons-material/Business";
+import ArticleIcon from "@mui/icons-material/Article";
+import PeopleIcon from "@mui/icons-material/People";
+import SettingsIcon from "@mui/icons-material/Settings";
+import FavoriteIcon from "@mui/icons-material/Favorite";
+import HistoryIcon from "@mui/icons-material/History";
 
-const NotificationItem = ({ text, time }) => (
-  <ListItem alignItems="flex-start" sx={{ px: 0 }}>
-    <ListItemText
-      primary={
-        <Typography variant="body2" fontWeight={500}>
-          {text}
-        </Typography>
-      }
-      secondary={
-        <Typography variant="caption" color="text.secondary">
-          {time}
-        </Typography>
-      }
-    />
-  </ListItem>
-);
+const drawerWidth = 240;
 
-const ActivityItem = ({ avatar, text, time }) => (
-  <ListItem alignItems="flex-start" sx={{ px: 0 }}>
-    <ListItemAvatar>
-      <Avatar alt="avatar" src={avatar} />
-    </ListItemAvatar>
-    <ListItemText
-      primary={<Typography variant="body2">{text}</Typography>}
-      secondary={
-        <Typography variant="caption" color="text.secondary">
-          {time}
-        </Typography>
-      }
-    />
-  </ListItem>
-);
+const Sidebar = () => {
+  const sections = [
+    {
+      title: "Favorites",
+      items: [
+        { label: "Overview", icon: <FavoriteIcon /> },
+        { label: "Projects", icon: <FolderIcon /> },
+      ],
+    },
+    {
+      title: "Dashboards",
+      items: [
+        { label: "Overview", icon: <DashboardIcon /> },
+        { label: "eCommerce", icon: <BusinessIcon /> },
+        { label: "Projects", icon: <FolderIcon /> },
+      ],
+    },
+    {
+      title: "Pages",
+      items: [
+        { label: "User Profile", icon: <AccountCircleIcon /> },
+        { label: "Overview", icon: <DashboardIcon /> },
+        { label: "Projects", icon: <FolderIcon /> },
+        { label: "Campaigns", icon: <ArticleIcon /> },
+        { label: "Documents", icon: <ArticleIcon /> },
+        { label: "Followers", icon: <PeopleIcon /> },
+      ],
+    },
+    {
+      title: "Account",
+      items: [{ label: "Settings", icon: <SettingsIcon /> }],
+    },
+    {
+      title: "Recently",
+      items: [{ label: "History", icon: <HistoryIcon /> }],
+    },
+  ];
 
-const ContactItem = ({ avatar, name }) => (
-  <ListItem sx={{ px: 0 }}>
-    <ListItemAvatar>
-      <Avatar alt={name} src={avatar} />
-    </ListItemAvatar>
-    <ListItemText primary={<Typography variant="body2">{name}</Typography>} />
-  </ListItem>
-);
-
-const RightPanel = () => {
   return (
-    <Box
-      width="18%"
-      minWidth={250}
-      px={2}
-      py={3}
+    <Drawer
+      variant="permanent"
       sx={{
-        bgcolor: "#f9f9fb",
-        borderLeft: "1px solid #e0e0e0",
-        height: "100vh",
+        width: drawerWidth,
+        flexShrink: 0,
+        [`& .MuiDrawer-paper`]: {
+          width: drawerWidth,
+          boxSizing: "border-box",
+          backgroundColor: "#f9fafb",
+          borderRight: "1px solid #e0e0e0",
+        },
       }}
     >
-      <Typography variant="h6" fontWeight={600} gutterBottom>
-        Notifications
-      </Typography>
-      <List dense>
-        <NotificationItem text="You fixed a bug." time="Just now" />
-        <NotificationItem text="New user registered." time="59 minutes ago" />
-        <NotificationItem text="You fixed a bug." time="12 hours ago" />
-        <NotificationItem
-          text="Andi Lane subscribed to you."
-          time="Today, 11:59 AM"
-        />
-      </List>
-
-      <Typography variant="h6" fontWeight={600} gutterBottom mt={3}>
-        Activities
-      </Typography>
-      <List dense>
-        <ActivityItem
-          avatar="https://i.pravatar.cc/30?img=1"
-          text="Changed the style."
-          time="Just now"
-        />
-        <ActivityItem
-          avatar="https://i.pravatar.cc/30?img=2"
-          text="Released a new version."
-          time="59 minutes ago"
-        />
-        <ActivityItem
-          avatar="https://i.pravatar.cc/30?img=3"
-          text="Submitted a bug."
-          time="12 hours ago"
-        />
-        <ActivityItem
-          avatar="https://i.pravatar.cc/30?img=4"
-          text="Modified A data in Page X."
-          time="Today, 11:59 AM"
-        />
-        <ActivityItem
-          avatar="https://i.pravatar.cc/30?img=5"
-          text="Deleted a page in Project X."
-          time="Feb 2, 2025"
-        />
-      </List>
-
-      <Typography variant="h6" fontWeight={600} gutterBottom mt={3}>
-        Contacts
-      </Typography>
-      <List dense>
-        <ContactItem
-          avatar="https://i.pravatar.cc/30?img=6"
-          name="Natali Craig"
-        />
-        <ContactItem avatar="https://i.pravatar.cc/30?img=7" name="Drew Cano" />
-        <ContactItem avatar="https://i.pravatar.cc/30?img=8" name="Andi Lane" />
-        <ContactItem
-          avatar="https://i.pravatar.cc/30?img=9"
-          name="Koray Okumus"
-        />
-        <ContactItem
-          avatar="https://i.pravatar.cc/30?img=10"
-          name="Kate Morrison"
-        />
-        <ContactItem
-          avatar="https://i.pravatar.cc/30?img=11"
-          name="Melody Macy"
-        />
-      </List>
-    </Box>
+      <Box sx={{ p: 2 }}>
+        <Typography variant="h6" fontWeight="bold">
+          ByeWind
+        </Typography>
+      </Box>
+      {sections.map((section, index) => (
+        <Box key={index}>
+          <Divider />
+          <List>
+            <Typography
+              variant="subtitle2"
+              sx={{ pl: 2, pt: 1 }}
+              color="text.secondary"
+            >
+              {section.title}
+            </Typography>
+            {section.items.map((item, idx) => (
+              <ListItem button key={idx}>
+                <ListItemIcon>{item.icon}</ListItemIcon>
+                <ListItemText primary={item.label} />
+              </ListItem>
+            ))}
+          </List>
+        </Box>
+      ))}
+    </Drawer>
   );
 };
 
-export default RightPanel;
+export default Sidebar;
